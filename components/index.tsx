b4ecf62213a6a0090357962e3139a9799fc36ed1
Shehryar_Kiyani <shehryar@xenex-media.com.au>
2023-10-02 11:20:59 +0500

    initial-commit

---
+++ components/index.tsx
@@ -0,0 +1,8 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import AuthProviders from "./AuthProviders";
+export {
+    Footer,
+    Navbar,
+    AuthProviders
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import { Footer,Navbar } from '@/components';
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Navbar/>
+  <Component {...pageProps} />
+  <Footer/>
+  </>
 }
